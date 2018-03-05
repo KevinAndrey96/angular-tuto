630be1a86d0dff3348fb9aebe102f355cd2ad5fa
Kevin <kahs_kevin@hotmail.com>
2018-03-05 00:46:09 -0500

    Completo

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AppPage } from './app.po';
+import { BlankPage } from './app.po';
 
-describe('angular-tuto App', () => {
-  let page: AppPage;
+describe('blank App', () => {
+  let page: BlankPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new BlankPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Tour of Heroes');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,11 @@
-import { browser, by, element } from 'protractor';
+import { browser, element, by } from 'protractor';
 
-export class AppPage {
+export class BlankPage {
   navigateTo() {
     return browser.get('/');
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('my-root h1')).getText();
   }
 }
